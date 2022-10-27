704f19a888af5229e16fa84236748d7fea4c36cc
KStoums <62219961+KStoums@users.noreply.github.com>
2022-10-27 20:47:23 +0200

    Added some extra code to task name change

--- commands/edit_command.go
+++ commands/edit_command.go
@@ -5,6 +5,7 @@ import (
 	"Terminal_Task_Managment_V2/messages"
 	"fmt"
 	"github.com/spf13/cobra"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -24,14 +25,50 @@ var editCommand = &cobra.Command{
 
 		for {
 			functions.ClearTerminal()
-
 			fmt.Print(fmt.Sprintf("%s \n%s \n%s \n%s \n%s \n%s \n\n%s", messages.HeaderEditCommand, messages.ChooseOneEdit, messages.ChooseTwoEdit,
 				messages.ChooseThreeEdit, messages.ChooseForEdit, messages.ChooseFiveEdit, messages.DefineEditChoose))
 			var chooseEditign string
 			fmt.Scan(&chooseEditign)
 
 			if strings.EqualFold(chooseEditign, "1") {
-				fmt.Print(messages.CommandEditSoon)
+				fmt.Print(messages.DefineIDTask)
+				var id string
+				fmt.Scan(&id)
+
+				if strings.EqualFold(id, "cancel") {
+					functions.ClearTerminal()
+					fmt.Print(messages.CancelEditTask)
+					return
+				}
+
+				strConv, err := strconv.Atoi(id)
+				if err != nil {
+					functions.ClearTerminal()
+					fmt.Print(messages.ErrorNotIntTask)
+					return
+				}
+
+				tasks := []TaskStruct{}
+
+				var found bool
+				for _, v := range tasks {
+					if v.Id == strConv {
+						found = true
+						fmt.Print(messages.EnterNewNameTask)
+						var newNameTask string
+						fmt.Scan(&newNameTask)
+
+						v.Name = newNameTask
+						fmt.Print("OK")
+						break
+					}
+				}
+
+				if !found {
+					functions.ClearTerminal()
+					fmt.Print(messages.TaskNotFoundSearhTask)
+					return
+				}
 				break
 			}
 
