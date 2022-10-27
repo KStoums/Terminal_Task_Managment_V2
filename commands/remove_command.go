2cbeeb611ad203c4f2d09617446500457cd5582d
KStoums <62219961+KStoums@users.noreply.github.com>
2022-10-27 19:42:39 +0200

    Adding remove command done

--- commands/remove_command.go
+++ commands/remove_command.go
@@ -4,10 +4,12 @@ import (
 	"Terminal_Task_Managment_V2/messages"
 	"encoding/json"
 	"fmt"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func init() {
@@ -45,12 +47,38 @@ var removeCommand = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		fmt.Print(messages.DefineIDTask)
+		//##### TABLEUR #####//
+		data := [][]string{}
+
+		for _, task := range tasks {
+			data = append(data, []string{fmt.Sprintf("%d", task.Id), task.Name, task.Status, task.Priority,
+				fmt.Sprintf("%d", task.Due), task.DateCreated.Format("2006-01-02 15:04:05")})
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"ID", "NAME", "STATUS", "PRIORITY", "DUE", "CREATION"})
+
+		for _, v := range data {
+			table.Append(v)
+		}
+		table.Render()
+		//##########//
+
+		fmt.Print("\n" + messages.DefineIDTask)
 		var id string
 
 		_, err = fmt.Scan(&id)
 		if err != nil {
 			log.Fatalln(err)
+		}
+
+		if strings.EqualFold(id, "cancel") {
+			for i := 0; i < 100; i++ {
+				fmt.Println("")
+			}
+
+			fmt.Print(messages.RemoveTaskCanceled)
+			return
 		}
 
 		strConv, err := strconv.Atoi(id)
@@ -59,13 +87,32 @@ var removeCommand = &cobra.Command{
 			return
 		}
 
+		var found bool
 		for i, v := range tasks {
-			if v.Id != strConv {
-				i++
-			} else if v.Id == strConv {
-				fmt.Println("Found !") //Remove Task
-				return
+			if v.Id == strConv {
+				tasks = append(tasks[:i], tasks[i+1:]...)
+				bytes, err := json.Marshal(tasks)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				found = true
+				err = os.WriteFile("./database/database.json", bytes, 644)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				for i := 0; i < 100; i++ {
+					fmt.Println("")
+				}
+
+				fmt.Println(fmt.Sprintf(">> Task [%d] has been deleted successfully!", strConv))
+				break
 			}
+		}
+
+		if !found {
+			fmt.Println(messages.TaskNotFoundSearhTask)
 		}
 	},
 }
